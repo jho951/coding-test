7562d40ca7929d427372d5fd8887d7c77006bafd
이장호 <jho951@naver.com>
2024-09-27 09:20:31 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.04 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -1,11 +1,5 @@
 class Solution {
-    public float solution(int num1, int num2) {
-        // num2가 0일 경우 예외 처리
-        if (num2 == 0) {
-            return -1; // 또는 다른 적절한 값이나 예외를 던질 수 있습니다.
-        }
-        
-        // 나눗셈 결과를 소수점 이하 3자리까지 계산
-        return (float)Math.floor((float) num1 / num2 * 1000);
+    public int solution(int num1, int num2) {
+        return (num1 *1000 ) / num2;
     }
 }
