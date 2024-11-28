9bf010e24aff311ecd2d69cf6773eb4918fa728f
이장호 <jho951@naver.com>
2024-11-28 14:06:39 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.java
+++ 프로그래머스/0/120829. 각도기/각도기.java
@@ -1,15 +1,15 @@
 class Solution {
     public int solution(int angle) {
-        int answer = 0;
-        if(angle > 0 && angle < 90){
-            answer = 1;
+        if(angle < 90){
+            return 1;
         }else if(angle == 90){
-            answer = 2;
+            return 2;
         }else if(angle < 180){
-            answer = 3;
+            return 3;
         }else if(angle == 180){
-            answer = 4;
-        };
-        return answer;
+            return 4;
+        }else{
+            return 0;
+        }
     }
 }
