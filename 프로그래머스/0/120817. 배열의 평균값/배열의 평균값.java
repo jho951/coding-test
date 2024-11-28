1a1d40264bdd945d4f9f8cc496fba48f17803c83
이장호 <jho951@naver.com>
2024-11-28 14:07:55 +0900

    [level 0] Title: 배열의 평균값, Time: 0.04 ms, Memory: 86.5 MB -BaekjoonHub

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
@@ -1,22 +1,9 @@
 class Solution {
     public double solution(int[] numbers) {
         double answer = 0;
-        for ( int i=0; i< numbers.length; i++){
+        for ( int i = 0; i< numbers.length; i++){
           answer+=  numbers[i];
         }
         return answer / numbers.length;
     }
 }
-
-// import java.util.Arrays;
-
-// class Solution {
-//     public double solution(int[] numbers) {
-//         double answer = 0;
-//         // 배열을 스트림으로 변환하여 순회
-//         Arrays.stream(numbers).forEach(item -> System.out.println(item));
-//         // 실제로 값을 계산하여 반환할 수 있도록 해야 함
-//         answer = Arrays.stream(numbers).average().orElse(0); // 예시로 평균 계산
-//         return answer;
-//     }
-// }
