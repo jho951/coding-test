a2ffa94bb6ce5805bbba00c541bf6de3adfa7006
이장호 <jho951@naver.com>
2024-12-24 17:04:10 +0900

    [level 0] Title: 양꼬치, Time: 0.03 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.java
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -2,9 +2,9 @@ class Solution {
     public int solution(int n, int k) {
         int answer = 0;
         if (n >= 10) {
-            answer = n * 12000 + k * 2000 - n / 10 * 2000;  // 기본 계산 + 할인 적용
+            answer = n * 12000 + k * 2000 - n / 10 * 2000;
         } else {
-            answer = n * 12000 + k * 2000;  // 기본 계산
+            answer = n * 12000 + k * 2000; 
         }
         return answer;
     }
