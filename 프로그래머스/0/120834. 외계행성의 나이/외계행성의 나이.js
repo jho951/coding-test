5b67345c551fc86cabd55e17de1456dff82a61fc
이장호 <jho951@naver.com>
2024-12-26 23:03:17 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -0,0 +1,14 @@
+function solution(age) {
+    var stringAge= String(age);
+        stringAge = stringAge.replaceAll("0","a")
+        stringAge = stringAge.replaceAll("1","b")
+        stringAge = stringAge.replaceAll("2","c")
+        stringAge = stringAge.replaceAll("3","d")
+        stringAge =  stringAge.replaceAll("4","e")
+        stringAge = stringAge.replaceAll("5","f")
+        stringAge = stringAge.replaceAll("6","g")
+        stringAge = stringAge.replaceAll("7","h")
+        stringAge = stringAge.replaceAll("8","i")
+        stringAge =  stringAge.replaceAll("9","j")
+    return stringAge;
+}
