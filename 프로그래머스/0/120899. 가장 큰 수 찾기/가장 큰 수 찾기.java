368e0679f237e075d1de63f1d8944b0ee48f988e
이장호 <jho951@naver.com>
2024-12-27 23:46:09 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.01 ms, Memory: 80.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int[] solution(int[] array) {
+        int max = Integer.MIN_VALUE; 
+        int index = -1; 
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i]; 
+                index = i;  
+            }
+        }
+        return new int[]{max, index};
+    }
+}
