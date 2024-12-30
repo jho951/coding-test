0e97b8ffe4947e48015cd336d7c1bc9bc97ba01e
이장호 <jho951@naver.com>
2024-12-30 23:50:45 +0900

    [level 0] Title: 모스부호 (1), Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -0,0 +1,15 @@
+function solution(letter) {
+    let answer = ""
+    const morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+    }
+    letter = letter.split(' ');
+    for(let i = 0; i < letter.length; i++){
+        answer += morse[letter[i]];
+    }
+    return answer;
+}
