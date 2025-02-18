f3bd87e1475f97c23bfd06793bbbf0ce9b0b3d8d
이장호 <jho951@naver.com>
2025-02-18 23:14:36 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.39 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -1,15 +1,14 @@
 class Solution {
     public int solution(String my_string) {
-        String[] aa = my_string.split(""); // 문자열을 배열로 분리
-        int answer = 0; // 결과 문자열 초기화
+        String[] wordSplit = my_string.split("");
+        int answer = 0;
         
-        for (int i = 0; i < aa.length; i++) {
-            char ch = aa[i].charAt(0); // 배열의 i번째 요소에서 첫 번째 문자를 가져옴
-            if (Character.isDigit(ch)) { // 숫자인지 확인
+        for (int i = 0; i < wordSplit.length; i++) {
+            char ch = wordSplit[i].charAt(0);
+            if (Character.isDigit(ch)) {
                 answer += Character.getNumericValue(ch);
             }
         }
-        
-        return answer; // 반복이 끝난 후 최종 결과 반환
+        return answer;
     }
 }
