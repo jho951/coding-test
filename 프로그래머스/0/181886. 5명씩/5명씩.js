e2ab73c25e0c491b042d5c38cec75307c46e2ab9
이장호 <jho951@naver.com>
2025-05-18 23:12:18 +0900

    [level 0] Title: 5명씩, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181886. 5명씩/5명씩.js
+++ 프로그래머스/0/181886. 5명씩/5명씩.js
@@ -1,8 +1,8 @@
 function solution(names) {
     var answer = [];
-    names.map((i,index)=>{
-        if(index%5===0){
-            answer.push(i)
+    names.map((i, index)=>{
+        if(index % 5 === 0){
+            answer.push(i);
         }
     })
     return answer;
