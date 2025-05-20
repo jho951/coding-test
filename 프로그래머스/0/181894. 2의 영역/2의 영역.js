3098025575401e5a33bb6da1654892975102c6fa
이장호 <jho951@naver.com>
2025-05-20 23:54:01 +0900

    [level 0] Title: 2의 영역, Time: 0.53 ms, Memory: 42.4 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.js
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.js
@@ -1,5 +1,5 @@
 function solution(arr) {
-    var first2= arr.indexOf(2)
-    var last2= arr.lastIndexOf(2)
+    var first2 = arr.indexOf(2);
+    var last2 = arr.lastIndexOf(2);
     return first2 === -1? [-1] : arr.slice(first2, last2 + 1);
 }
