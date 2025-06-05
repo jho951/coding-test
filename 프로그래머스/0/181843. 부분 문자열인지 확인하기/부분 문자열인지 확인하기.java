4a1bfcdc895f29eff4030b204337ce8bca4c2383
이장호 <jho951@naver.com>
2025-06-06 01:34:28 +0900

    [level 0] Title: 부분 문자열인지 확인하기, Time: 0.15 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(String my_string, String target) {
+        int len1 = my_string.length();
+        int len2 = target.length();
+        for(int i = 0; i<= len1 - len2; i++){
+            boolean match = true;
+            for(int j = 0; j< len2; j++){
+                if (my_string.charAt(i + j) != target.charAt(j)) {
+                    match = false;
+                    break;
+                }
+            }
+             if (match) return 1;
+        }
+
+        return 0;
+    }
+}
