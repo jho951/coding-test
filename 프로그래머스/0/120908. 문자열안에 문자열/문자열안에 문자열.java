5200af57b250ec8c057daf6f24e9c73603dcbc8d
이장호 <jho951@naver.com>
2025-06-06 00:49:48 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.04 ms, Memory: 87.3 MB -BaekjoonHub

--- 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.java
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.java
@@ -1,6 +1,20 @@
-import java.util.*;
 class Solution {
     public int solution(String str1, String str2) {
-        return str1.contains(str2)? 1 : 2;
+        int len1 = str1.length();
+        int len2 = str2.length();
+
+        // str1에서 str2가 시작할 수 있는 모든 위치를 순회
+        for (int i = 0; i <= len1 - len2; i++) {
+            boolean match = true;
+            for (int j = 0; j < len2; j++) {
+                if (str1.charAt(i + j) != str2.charAt(j)) {
+                    match = false;
+                    break;
+                }
+            }
+            if (match) return 1;  // str2가 str1 안에 있음
+        }
+
+        return 2;  // 포함되지 않음
     }
 }
