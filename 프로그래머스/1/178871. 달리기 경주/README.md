85c3e2fe3b83e0ba1f49c4100948ea1baabcf651
이장호 <jho951@naver.com>
2025-06-26 23:46:35 +0900

    [level 1] Title: 달리기 경주, Time: 350.21 ms, Memory: 382 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/README.md
+++ 프로그래머스/1/178871. 달리기 경주/README.md
@@ -1,10 +1,10 @@
 # [level 1] 달리기 경주 - 178871 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/178871?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/178871) 
 
 ### 성능 요약
 
-메모리: 352 MB, 시간: 299.19 ms
+메모리: 382 MB, 시간: 350.21 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 20일 22:48:46
+2025년 06월 26일 23:46:32
 
 ### 문제 설명
 
--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -6,31 +6,25 @@ class Solution {
     public String[] solution(String[] players, String[] callings) {
         HashMap<String, Integer> playerMap = new HashMap<>();
         List<String> playerList = new ArrayList<>();
-
         // 플레이어와 인덱스를 해시 맵에 저장
         for (int i = 0; i < players.length; i++) {
             playerMap.put(players[i], i);
             playerList.add(players[i]);
         }
-
         // 호출된 플레이어 처리
         for (String calling : callings) {
             Integer idx = playerMap.get(calling);
-            
             // 인덱스가 0보다 클 경우에만 교환 수행
             if (idx != null && idx > 0) {
                 String prevPlayer = playerList.get(idx - 1);
-
                 // 플레이어와 이전 플레이어의 인덱스 교환
                 playerList.set(idx, prevPlayer);
                 playerList.set(idx - 1, calling);
-
                 // 해시 맵 업데이트
                 playerMap.put(calling, idx - 1);
                 playerMap.put(prevPlayer, idx);
             }
         }
-
         // 결과 배열로 변환
         return playerList.toArray(new String[0]);
     }
