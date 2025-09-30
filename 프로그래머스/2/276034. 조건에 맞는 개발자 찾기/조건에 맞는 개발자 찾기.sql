529164a3b84a51ee80858588b817deb45ef4add1
이장호 <jho951@naver.com>
2025-09-30 23:30:45 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -0,0 +1,9 @@
+select distinct(id), email, first_name, last_name
+from developers as d 
+join (
+    select *
+    from skillcodes
+    where name = 'Python' or name = 'C#'
+) as s
+on (d.skill_code & s.code) = s.code
+order by id
