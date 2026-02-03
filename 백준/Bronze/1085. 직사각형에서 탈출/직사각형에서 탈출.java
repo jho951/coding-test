eed9244268ab9666c252efc960828a3d656c9c01
이장호 <jho951@naver.com>
2026-02-03 16:15:35 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 100 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.java
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.java
@@ -1,16 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static String next() throws IOException {
+		while (st == null || !st.hasMoreTokens())
+			st = new StringTokenizer(br.readLine());
+		return st.nextToken();
+	}
+	static int nextInt() throws IOException {
+		return Integer.parseInt(next());
+	}
 	public static void main(String[] args)throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] input = br.readLine().split(" ");
-		int[] mx = new int[4];
-		mx[0] = Integer.parseInt(input[0]);
-		mx[1] = Integer.parseInt(input[1]);
-		mx[2] = Integer.parseInt(input[2]) - Integer.parseInt(input[0]);
-		mx[3] = Integer.parseInt(input[3]) - Integer.parseInt(input[1]);
-		System.out.println(Math.min(Math.min(mx[0], mx[1]), Math.min(mx[2], mx[3])));
+		int[] curPos = {nextInt(), nextInt()};
+		int[] start = {0,0};
+		int[] end = {nextInt(), nextInt()};
+
+		System.out.println(Math.min(Math.min(curPos[0] - start[0], end[0] - curPos[0]), Math.min(curPos[1] - start[1], end[1] - curPos[1])));
 	}
 }
