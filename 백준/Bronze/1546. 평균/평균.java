0a48d23915565d78d46b57fd2da705308be18ed1
이장호 <jho951@naver.com>
2026-02-04 16:37:56 +0900

    [Bronze I] Title: 평균, Time: 116 ms, Memory: 14424 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,22 +1,29 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
+
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int N = input.nextInt();
-        if (N > 1000) {
-            throw new ArithmeticException();
-        }
-        int[] A = new int[N];
-        for(int i = 0; i < N; i++) {
-            A[i] = input.nextInt();
-        }
-        long sum = 0;
-        long max = 0;
-        for(int i = 0; i < N; i++) {
-            if(A[i] > max) max = A[i];
-            sum += A[i];
-        }
-        System.out.print(sum*100.0 / max / N);
-    }
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static String next() throws IOException {
+		while (st == null || !st.hasMoreTokens())
+			st = new StringTokenizer(br.readLine());
+		return st.nextToken();
+	}
+	static int nextInt() throws IOException {
+		return Integer.parseInt(next());
+	}
+	public static void main(String[] args) throws IOException {
+		int N = nextInt();
+		int[] scores = new int[N];
+		long result = 0;
+		for (int i = 0; i < N; i++) {
+			scores[i] = nextInt();
+			result += scores[i];
+		}
+		int max = Arrays.stream(scores).max().getAsInt();
+
+
+		System.out.println((result * 100.0) / max / N);
+	}
 }
