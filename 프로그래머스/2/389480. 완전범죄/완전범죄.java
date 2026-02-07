b141069e49024f91bf8ddf9e7bb88146b57a9759
이장호 <jho951@naver.com>
2026-02-08 00:49:06 +0900

    [level 2] Title: 완전범죄, Time: 0.23 ms, Memory: 87.6 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.java
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.java
@@ -2,9 +2,8 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] info, int n, int m) {
-        // dp[j]는 B의 흔적이 j일 때 A 흔적의 최솟값
         int[] dp = new int[m];
-        int INF = 1000000; // 충분히 큰 값 (n의 최대가 120,000이므로)
+        int INF = 1000000;
         Arrays.fill(dp, INF);
         dp[0] = 0;
 
@@ -12,20 +11,15 @@ class Solution {
             int aTrace = item[0];
             int bTrace = item[1];
 
-            // 뒤에서부터 계산해야 이전 물건의 결과에 영향을 받지 않음 (1차원 DP 기법)
             for (int j = m - 1; j >= 0; j--) {
-                // 1. B가 훔치는 경우: j >= bTrace일 때만 가능
                 if (j >= bTrace) {
                     dp[j] = Math.min(dp[j] + aTrace, dp[j - bTrace]);
                 } 
-                // 2. A가 훔치는 경우: 항상 가능하지만 A의 흔적만 추가됨
                 else {
                     dp[j] = dp[j] + aTrace;
                 }
             }
         }
-
-        // 결과 탐색: A의 흔적이 n 미만인 경우 중 최솟값 찾기
         int answer = INF;
         for (int j = 0; j < m; j++) {
             if (dp[j] < n) {
