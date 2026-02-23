493391c4254d2435453c9ba2995e75067b2b2208
이장호 <jho951@naver.com>
2026-02-23 23:48:03 +0900

    [Bronze II] Title: 커트라인, Time: 104 ms, Memory: 14504 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -0,0 +1,26 @@
+/**
+ * 기본 템플릿
+ */
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static String next() throws IOException {
+		while (st == null || !st.hasMoreTokens()) st = new StringTokenizer(br.readLine());
+		return st.nextToken();
+	}
+	static int nextInt() throws IOException { return Integer.parseInt(next()); }
+
+	public static void main(String[] args) throws Exception {
+		int N = nextInt();
+		int M = nextInt();
+		int[] list = new int[N];
+		for (int i = 0; i < N; i++) {
+			list[i] = nextInt();
+		}
+		Arrays.sort(list);
+		System.out.println(list[list.length -  M]);
+	}
+}
