bfb633c308125126799f97c80f45740fb0d07c20
이장호 <jho951@naver.com>
2026-03-06 17:55:59 +0900

    [Bronze II] Title: 블랙잭, Time: 120 ms, Memory: 14392 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,44 +1,41 @@
+/**
+ * 기본 템플릿
+ */
+import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static String next() throws IOException {
+		while (st == null || !st.hasMoreTokens()) st = new StringTokenizer(br.readLine());
+		return st.nextToken();
+	}
+	static int nextInt() throws IOException { return Integer.parseInt(next()); }
 
-		int N = sc.nextInt();
-		int M = sc.nextInt();
-		int[] cards = new int[N];
+	public static void main(String[] args) throws Exception {
+		int N = nextInt();
+		int M = nextInt();
+		int[] list = new int[N];
 
-		for (int i = 0; i < N; i++) {
-			cards[i] = sc.nextInt();
+		int answer = 0;
+		for(int i = 0; i< list.length; i++){
+			list[i] = nextInt();
 		}
-
-		Arrays.sort(cards);
-
-		int result = 0;
-
-		for (int i = 0; i < N - 2; i++) {
-			int left = i + 1;
-			int right = N - 1;
-
-			while (left < right) {
-				int sum = cards[i] + cards[left] + cards[right];
-				// 값이 같으면 더 이상 필요없다.
-				if (sum == M) {
-					System.out.println(M);
-					return;
-				}
-				// 크면
-				if (sum < M) {
-					result = Math.max(result, sum);
-					left++;
-				}
-				// 작으면
-				else {
-					right--;
+		Arrays.sort(list);
+		for(int i = 0; i< list.length; i++){
+			for(int j = i+1; j< list.length; j++){
+				for(int k = j+1; k< list.length; k++){
+					if(list[i] + list[j] + list[k] == M){
+						answer = M;
+					}else{
+						if(list[i] + list[j] + list[k] < M){
+							answer = Math.max(answer, list[i] + list[j] + list[k]);
+						}
+					}
 				}
 			}
 		}
-
-		System.out.println(result);
+		System.out.println(answer);
 	}
 }
