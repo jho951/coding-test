e389b26cb7909eb1a7431608b43d17a4dae3f34e
이장호 <jho951@naver.com>
2026-03-08 18:33:29 +0900

    [Silver V] Title: 나이순 정렬, Time: 580 ms, Memory: 46248 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,48 +1,54 @@
+/**
+ * 기본 템플릿
+ */
 import java.io.*;
 import java.util.*;
 
 public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static String next() throws IOException {
+		while (st == null || !st.hasMoreTokens()) st = new StringTokenizer(br.readLine());
+		return st.nextToken();
+	}
+	static int nextInt() throws IOException { return Integer.parseInt(next()); }
 
-	// 멤버 정보를 담을 클래스 정의
-	static class Member {
+	static class Member{
 		int age;
 		String name;
-		int idx; // 입력 순서 저장 (사실 Java 정렬은 Stable해서 필수까진 아님)
-
-		public Member(int age, String name, int idx) {
+		int id;
+		public Member(int age, String name,int id) {
 			this.age = age;
 			this.name = name;
-			this.idx = idx;
+			this.id = id;
 		}
 	}
 
 	public static void main(String[] args) throws Exception {
-		int N = Integer.parseInt(br.readLine());
+		int N = nextInt();
 		Member[] members = new Member[N];
-
 		for (int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int age = Integer.parseInt(st.nextToken());
-			String name = st.nextToken();
+			int age = nextInt();
+			String name = next();
 			members[i] = new Member(age, name, i);
 		}
-
-		// 정렬 로직
-		Arrays.sort(members, (m1, m2) -> {
-			// 1. 나이 순으로 오름차순 정렬
-			if (m1.age != m2.age) {
-				return m1.age - m2.age;
-			}
-			// 2. 나이가 같다면? 입력 순서(idx)대로 정렬
-			return m1.idx - m2.idx;
+		Arrays.sort(members,(m1,m2)-> {
+				if(m1.age != m2.age){
+					return m1.age - m2.age;
+				}
+				return m1.id - m2.id;
 		});
-
-		// 결과 출력
 		StringBuilder sb = new StringBuilder();
-		for (Member m : members) {
-			sb.append(m.age).append(" ").append(m.name).append("\n");
-		}
-		System.out.print(sb);
+			for (Member m : members) {
+				sb.append(m.age).append(" ").append(m.name).append("\n");
+			}
+			System.out.print(sb);
 	}
 }
+
+
+
+
+
+
+
