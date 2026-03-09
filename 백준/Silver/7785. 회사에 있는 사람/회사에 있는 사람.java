b91fee50d73b6e97c457c574e13d33a9bfdcd2c2
이장호 <jho951@naver.com>
2026-03-09 14:12:03 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 648 ms, Memory: 49880 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -0,0 +1,31 @@
+/**
+ * 기본 템플릿
+ */
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static String next() throws IOException {
+		while (st == null || !st.hasMoreTokens()) st = new StringTokenizer(br.readLine());
+		return st.nextToken();
+	}
+	static int nextInt() throws IOException { return Integer.parseInt(next()); }
+
+	public static void main(String[] args) throws Exception {
+		int N = nextInt();
+		Map<String, String>map = new HashMap<>();
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < N; i++){
+			String person = next();
+			String status = next();
+			if (status.equals("leave")) {map.remove(person);}
+			else {map.put(person, status);}
+		}
+		List<String> remaining = new ArrayList<>(map.keySet());
+		Collections.sort(remaining, Collections.reverseOrder());
+		for(String person : remaining){sb.append(person).append("\n");}
+		System.out.println(sb);
+	}
+}
