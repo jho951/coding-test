f597998e3b97bae4728fd269a8f1d92650062014
이장호 <jho951@naver.com>
2026-03-13 00:34:20 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 648 ms, Memory: 226744 KB -BaekjoonHub

--- 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.java
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.java
@@ -1,26 +1,27 @@
+/**
+ * 기본 템플릿
+ * 1005번 문제
+ */
 import java.io.*;
 import java.util.*;
 
-public class Main { 
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static String next() throws IOException {
+		while (st == null || !st.hasMoreTokens()) st = new StringTokenizer(br.readLine());
+		return st.nextToken();
+	}
 
-    public static void main(String[] args) throws Exception {
-        // 1. 입력 받기
-        String input = br.readLine();
-        
-        // 2. 중복을 제거하며 저장할 Set 선언
-        Set<String> set = new HashSet<>();
-
-        // 3. 모든 부분 문자열 찾기 (이중 반복문)
-        // i는 시작점, j는 끝점
-        for (int i = 0; i < input.length(); i++) {
-            for (int j = i + 1; j <= input.length(); j++) {
-                // substring(i, j)는 i인덱스부터 j-1인덱스까지 자름
-                set.add(input.substring(i, j));
-            }
-        }
-
-        // 4. Set의 크기가 곧 서로 다른 부분 문자열의 개수
-        System.out.println(set.size());
-    }
+	public static void main(String[] args) throws Exception {
+		StringBuilder sb = new StringBuilder();
+		sb.append(next());
+		Set<String> set = new HashSet<>();
+		for(int i = 0; i< sb.length(); i++) {
+			for(int j = i + 1; j<= sb.length(); j++) {
+				set.add(sb.substring(i, j));
+			}
+		}
+		System.out.println(set.size());
+	}
 }
