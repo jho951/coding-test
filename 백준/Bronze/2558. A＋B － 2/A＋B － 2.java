0b26f52a9827049a835927c2d5e1dd2128db81e0
이장호 <jho951@naver.com>
2026-03-22 00:38:02 +0900

    [Bronze V] Title: A+B - 2, Time: 108 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+
+	static String next() throws IOException {
+		while (st == null || !st.hasMoreElements()) {
+			st = new StringTokenizer(br.readLine());
+		}
+		return st.nextToken();
+	}
+
+	static int nextInt() throws IOException {
+		return Integer.parseInt(next());
+	}
+
+
+	public static void main(String[] args) throws Exception {
+		System.out.print(nextInt() + nextInt());
+	}
+}
