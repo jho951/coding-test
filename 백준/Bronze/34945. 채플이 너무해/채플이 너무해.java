bdb02ed3abb1f7c17d82511f5673eaba5a6983ed
이장호 <jho951@naver.com>
2026-03-26 19:15:45 +0900

    [Bronze V] Title: 채플이 너무해, Time: 96 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/34945. 채플이 너무해/채플이 너무해.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+
+	static String next() throws IOException {
+		while (st == null || !st.hasMoreElements()) {
+			String line = br.readLine();
+			st = new StringTokenizer(line);
+		}
+		return st.nextToken();
+	}
+
+	static int nextInt() throws IOException {
+		return Integer.parseInt(next());
+	}
+
+	public static void main(String[] args) throws Exception {
+		int N = nextInt();
+		if(N >= 6){
+			System.out.println("Success!");
+		}else{
+			System.out.println("Oh My God!");
+		}
+	}
+}
