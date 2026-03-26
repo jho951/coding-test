97264773d85e3c058936c6d9b8d5d5ad46d72962
이장호 <jho951@naver.com>
2026-03-26 18:19:53 +0900

    [Bronze V] Title: 상근이의 친구들, Time: 100 ms, Memory: 13976 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+
+	static String next() throws IOException {
+		while (st == null || !st.hasMoreElements()) {
+			String line = br.readLine();
+			st = new StringTokenizer(line);
+		}
+		return st.nextToken();
+	}
+
+	static int nextInt() throws IOException {
+		return Integer.parseInt(next());
+	}
+
+	public static void main(String[] args) throws Exception {
+		while(true){
+			int N = nextInt();
+			int M = nextInt();
+			if(N == 0 && M == 0)break;
+			System.out.println(N + M);
+		}
+
+	}
+}
