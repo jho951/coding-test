1453d519263574fafd6121b48409b56d906cec13
이장호 <jho951@naver.com>
2026-03-26 19:58:56 +0900

    [Bronze V] Title: 가희와 교통 요금, Time: 100 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/34665. 가희와 교통 요금/가희와 교통 요금.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+
+	static String next() throws IOException {
+		while (st == null || !st.hasMoreElements()) {
+			String line = br.readLine();
+			st = new StringTokenizer(line);
+		}
+		return st.nextToken();
+	}
+
+	static int nextInt() throws IOException {
+		return Integer.parseInt(next());
+	}
+
+	public static void main(String[] args) throws Exception {
+		String str1 = next();
+		String str2 = next();	
+		if(str1.equals(str2))
+			System.out.println(0);
+		else
+			System.out.println(1550);
+	}
+}
