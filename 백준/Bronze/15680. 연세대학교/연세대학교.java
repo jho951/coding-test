68185311bf6a2815e8c3f502553038b6b8e3363c
이장호 <jho951@naver.com>
2026-03-28 22:09:24 +0900

    [Bronze V] Title: 연세대학교, Time: 104 ms, Memory: 14124 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+
+	static String next() throws IOException {
+		while (st == null || !st.hasMoreElements()) {
+			String line = br.readLine();
+			st = new StringTokenizer(line);
+		}
+		return st.nextToken();
+	}
+
+	static int nextInt() throws IOException {
+		return Integer.parseInt(next());
+	}
+
+	public static void main(String[] args) throws Exception {
+		int N = nextInt();
+		if( N == 0) {
+			System.out.println("YONSEI");
+		}else if( N == 1) {
+			System.out.println("Leading the Way to the Future");
+		}
+	}
+}
