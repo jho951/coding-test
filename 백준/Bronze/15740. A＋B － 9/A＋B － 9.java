f41c82d084ae7a7976634314b34d3c5c52c9b496
이장호 <jho951@naver.com>
2026-03-29 23:29:26 +0900

    [Bronze V] Title: A+B - 9, Time: 204 ms, Memory: 17484 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/15740. A＋B － 9/A＋B － 9.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+
+	static String next() throws IOException {
+		while (st == null || !st.hasMoreElements()) {
+			String line = br.readLine();
+			st = new StringTokenizer(line);
+		}
+		return st.nextToken();
+	}
+
+	static int nextInt() throws IOException {
+		return Integer.parseInt(next());
+	}
+
+	public static void main(String[] args) throws Exception {
+		BigInteger a = new BigInteger(next());
+		BigInteger b = new BigInteger(next());
+		System.out.println(a.add(b));
+	}
+}
