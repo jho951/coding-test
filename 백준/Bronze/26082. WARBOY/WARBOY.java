1d1e4e6bf570ba0d176e92cd7ec69b1f092b6763
이장호 <jho951@naver.com>
2026-04-03 19:48:53 +0900

    [Bronze V] Title: WARBOY, Time: 112 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/26082. WARBOY/WARBOY.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+
+	static String next() throws IOException {
+		while (st == null || !st.hasMoreElements()) {
+			String line = br.readLine();
+			st = new StringTokenizer(line);
+		}
+		return st.nextToken();
+	}
+
+	static int nextInt() throws IOException {
+		return Integer.parseInt(next());
+	}
+
+	public static void main(String[] args) throws Exception {
+		int N = nextInt();
+		int M = nextInt();
+		int K = nextInt();
+		int compare = M / N;
+		System.out.println(compare * K * 3);
+	}
+}
