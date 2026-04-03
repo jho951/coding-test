cceb83b2ea7dd1136c3ef522a53d298eb6b90e29
이장호 <jho951@naver.com>
2026-04-03 19:41:54 +0900

    [Bronze V] Title: 13 Floors, Time: 100 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Bronze/34412. 13 Floors/13 Floors.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+
+	static String next() throws IOException {
+		while (st == null || !st.hasMoreElements()) {
+			String line = br.readLine();
+			st = new StringTokenizer(line);
+		}
+		return st.nextToken();
+	}
+
+	static int nextInt() throws IOException {
+		return Integer.parseInt(next());
+	}
+
+	public static void main(String[] args) throws Exception {
+		int N = nextInt();
+		if (N >= 13){
+			System.out.print(N + 1);
+		}else{
+			System.out.print(N);
+		}
+
+	}
+}
