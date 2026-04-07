d5284c3a30aeaeb564e75281d487129cf0224c95
이장호 <jho951@naver.com>
2026-04-07 23:42:44 +0900

    [Bronze V] Title: Serca, Time: 3784 ms, Memory: 70012 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+
+	static String next() throws IOException {
+		while (st == null || !st.hasMoreElements()) {
+			String line = br.readLine();
+			st = new StringTokenizer(line);
+		}
+		return st.nextToken();
+	}
+
+	static int nextInt() throws IOException {
+		return Integer.parseInt(next());
+	}
+
+	static long nextLong() throws IOException {
+		return Long.parseLong(next());
+	}
+	static double nextDouble() throws IOException {
+		return Double.parseDouble(next());
+	}
+
+	public static void main(String[] args) throws Exception {
+		int N = nextInt();
+		for (int i = 0; i < N; i++) {
+			System.out.println(" @@@   @@@ ");
+			System.out.println("@   @ @   @");
+			System.out.println("@    @    @");
+			System.out.println("@         @");
+			System.out.println(" @       @ ");
+			System.out.println("  @     @  ");
+			System.out.println("   @   @   ");
+			System.out.println("    @ @    ");
+			System.out.println("     @     ");
+		}
+	}
+}
