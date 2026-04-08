551e70fcc8dfea65248fa859bdffbdcab339fb7b
이장호 <jho951@naver.com>
2026-04-09 00:27:30 +0900

    [Bronze V] Title: 時間 (Hour), Time: 104 ms, Memory: 14116 KB -BaekjoonHub

---
+++ 백준/Bronze/27327. 時間 （Hour）/時間 （Hour）.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+
+	static String next() throws IOException {
+		while (st == null || !st.hasMoreElements()) {
+			String line = br.readLine();
+			st = new StringTokenizer(line);
+		}
+		return st.nextToken();
+	}
+
+	static int nextInt() throws IOException {
+		return Integer.parseInt(next());
+	}
+
+	public static void main(String[] args) throws Exception {
+		int N = nextInt();
+		System.out.println(N * 24);
+	}
+}
