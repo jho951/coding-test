aee7f4e1d7baa7a20e951a1a02b2cc15b2115cb7
이장호 <jho951@naver.com>
2026-04-09 18:34:00 +0900

    [Bronze V] Title: 所持金 (Money On Me), Time: 108 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Bronze/33169. 所持金 （Money On Me）/所持金 （Money On Me）.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+
+	static String next() throws IOException {
+		while (st == null || !st.hasMoreElements()) {
+			String line = br.readLine();
+			st = new StringTokenizer(line);
+		}
+		return st.nextToken();
+	}
+
+	static int nextInt() throws IOException {
+		return Integer.parseInt(next());
+	}
+	static float nextFloat() throws IOException {
+		return Float.parseFloat(next());
+	}
+
+	public static void main(String[] args) throws Exception {
+		int A = nextInt();
+		int B = nextInt();
+		System.out.println(A * 1000 + B * 10000);
+	}
+}
