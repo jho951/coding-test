4ccc3a29b3a753a33b83d5b61dc0b6ea2cf53d1e
이장호 <jho951@naver.com>
2026-04-09 12:44:08 +0900

    [Bronze V] Title: 초코바, Time: 104 ms, Memory: 14320 KB -BaekjoonHub

---
+++ 백준/Bronze/27959. 초코바/초코바.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+
+	static String next() throws IOException {
+		while (st == null || !st.hasMoreElements()) {
+			String line = br.readLine();
+			st = new StringTokenizer(line);
+		}
+		return st.nextToken();
+	}
+
+	static int nextInt() throws IOException {
+		return Integer.parseInt(next());
+	}
+
+	public static void main(String[] args) throws Exception {
+		int N = 100 * nextInt();
+		int price = nextInt();
+		if(price > N){
+			System.out.println("No");
+		}else{
+			System.out.println("Yes");
+		}
+	}
+}
