d73b521c73aa201f9bd23c25feb431f996e9d996
이장호 <jho951@naver.com>
2026-04-09 11:35:49 +0900

    [Bronze IV] Title: R2, Time: 104 ms, Memory: 14124 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+
+	static String next() throws IOException {
+		while (st == null || !st.hasMoreElements()) {
+			String line = br.readLine();
+			st = new StringTokenizer(line);
+		}
+		return st.nextToken();
+	}
+
+	static int nextInt() throws IOException {
+		return Integer.parseInt(next());
+	}
+
+	public static void main(String[] args) throws Exception {
+		int R1 = nextInt();
+		int S = nextInt();
+		System.out.print(2 * S - R1);
+	}
+}
