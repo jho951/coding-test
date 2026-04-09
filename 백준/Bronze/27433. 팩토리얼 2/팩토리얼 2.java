0bc811a554d5a683937c59191cc47215ad2c7f97
이장호 <jho951@naver.com>
2026-04-09 12:19:27 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 112 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+
+	static String next() throws IOException {
+		while (st == null || !st.hasMoreElements()) {
+			String line = br.readLine();
+			st = new StringTokenizer(line);
+		}
+		return st.nextToken();
+	}
+
+	static int nextInt() throws IOException {
+		return Integer.parseInt(next());
+	}
+
+	public static void main(String[] args) throws Exception {
+		int N = nextInt();
+		long answer = 1;
+		for(int i = 1; i <= N; i++) {
+			answer *= i;
+		}
+		System.out.println(answer);
+	}
+}
