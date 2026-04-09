7bdef0e07917afc345026b1ba8b7dc75e11b047d
이장호 <jho951@naver.com>
2026-04-09 12:07:26 +0900

    [Bronze IV] Title: 줄번호, Time: 128 ms, Memory: 16064 KB -BaekjoonHub

---
+++ 백준/Bronze/4470. 줄번호/줄번호.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+
+	static String next() throws IOException {
+		while (st == null || !st.hasMoreElements()) {
+			String line = br.readLine();
+			st = new StringTokenizer(line);
+		}
+		return st.nextToken();
+	}
+
+	static int nextInt() throws IOException {
+		return Integer.parseInt(next());
+	}
+
+	public static void main(String[] args) throws Exception {
+		int N = nextInt();
+		for(int i = 0; i < N; i++) {
+			String str = br.readLine();
+			System.out.println(i+1 + ". " + str);
+		}
+	}
+}
