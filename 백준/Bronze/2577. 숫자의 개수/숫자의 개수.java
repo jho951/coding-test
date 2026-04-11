c709aa645adfb8d1d28686d65eb1e7dfc67b707f
이장호 <jho951@naver.com>
2026-04-11 20:05:27 +0900

    [Bronze II] Title: 숫자의 개수, Time: 104 ms, Memory: 14096 KB -BaekjoonHub

---
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        int C = Integer.parseInt(br.readLine());
+        
+        String s = String.valueOf(A * B * C);
+        int[] arr = new int[10];
+        
+        for(int i = 0; i < s.length(); i++) {
+            arr[s.charAt(i) - '0']++;
+        }
+        
+        for(int i = 0; i < 10; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
