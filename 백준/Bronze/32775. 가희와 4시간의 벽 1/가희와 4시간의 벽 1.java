494fb6088f636905ac42ba2d26e82a4fdb7a1d1f
이장호 <jho951@naver.com>
2026-04-11 19:47:29 +0900

    [Bronze V] Title: 가희와 4시간의 벽 1, Time: 100 ms, Memory: 14084 KB -BaekjoonHub

---
+++ 백준/Bronze/32775. 가희와 4시간의 벽 1/가희와 4시간의 벽 1.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static String next() throws IOException {
+		while (st == null || !st.hasMoreElements()) {
+			String line = br.readLine();
+			st = new StringTokenizer(line);
+		}
+		return st.nextToken();
+	}
+	static int nextInt() throws IOException {
+		return Integer.parseInt(next());
+	}
+	static long nextLong() throws IOException {
+		return Long.parseLong(next());
+	}
+
+	public static void main(String[] args) throws IOException {
+		int A = nextInt();
+		int B = nextInt();
+		if(A > B){
+			System.out.println("flight");
+		}else{
+			System.out.println("high speed rail");
+		}
+	}
+}
