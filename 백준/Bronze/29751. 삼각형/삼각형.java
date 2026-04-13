d6882cc07ec7d99d0f7ea528f51f38e399bec32f
이장호 <jho951@naver.com>
2026-04-13 17:34:16 +0900

    [Bronze V] Title: 삼각형, Time: 188 ms, Memory: 17948 KB -BaekjoonHub

---
+++ 백준/Bronze/29751. 삼각형/삼각형.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		float W = sc.nextFloat();
+		float H = sc.nextFloat();
+		System.out.println((W * H) / 2);
+	}
+}
