4b88872a05bb43933b82077e73702dc8e767a836
이장호 <jho951@naver.com>
2026-04-24 22:57:36 +0900

    [Bronze V] Title: 라면 공식, Time: 104 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/30007. 라면 공식/라면 공식.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static String next() throws IOException {
+		while (st == null || !st.hasMoreElements()) {
+			String line = br.readLine();
+			st = new StringTokenizer(line);
+		}
+		return st.nextToken();
+	}
+	static int nextInt() throws IOException {
+		return Integer.parseInt(next());
+	}
+	static int STICK_LENGTH = 64;
+
+
+	public static void main(String[] args) throws IOException {
+		int N = nextInt();
+		for(int i = 0; i < N; i++) {
+			int A = nextInt();
+			int B = nextInt();
+			int X = nextInt();
+			System.out.println(A * (X - 1) + B);
+		}
+	}
+}
